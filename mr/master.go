8560b4c95522e0db186a3b0f69a28a119444c314
liyefei737 <macrofly@gmail.com>
2020-05-19 16:10:55 -0700

    master done

--- mr/master.go
+++ mr/master.go
@@ -12,36 +12,21 @@ import (
 )
 
 type Master struct {
-	phase              string   // "map" or "reduce"
-	splits             []string // paper refers to a input partition as a split, which is the input to a map function
-	NReduce            int
-	mapTaskStatus      map[int]*MapStatus
-	inProgressMapTasks map[int]bool // a "set" of map tasks that are in progress
-	lock               sync.Mutex
-	wg                 sync.WaitGroup
+	phase             string   // "map" or "reduce", or "done"  done means all map and reduce tasks are all finished
+	splits            []string // paper refers to a input partition as a split, which is the input to a map function
+	NReduce           int
+	mapTasksStatus    map[int]*TaskStatus
+	reduceTasksStatus map[int]*TaskStatus
+	inProgressTasks   map[int]bool // a "set" of tasks(either all map tasks or reduce tasks) that are in progress
+	mapTasksTodo      int
+	reduceTasksTodo   int
+	GetTaskLock       sync.Mutex
+	ReportDoneLock    sync.Mutex
 }
 
-func (master *Master) setMapReply(reply *MasterMessage) {
-
-	for taskID, taskStatus := range master.mapTaskStatus {
-		if taskStatus.status == "notStarted" {
-			taskStatus.status = "inProgress"
-			taskStatus.startTime = time.Now()
-			master.inProgressMapTasks[taskID] = true
-
-			reply.Task = "map"
-			reply.TaskID = taskID
-			reply.NReduce = master.NReduce
-			return
-		}
-	}
-
-	// all tasks are either in inProgress(a worker is working on it) or finished state
-	// we make this worker to run the first reduce, and block untill all other map tasks are finished
-	master.wg.Wait()
-	master.phase = "reduce"
-	reply.Task = "skip"
-
+type TaskStatus struct {
+	status    string // notStarted,inProgress, or finished
+	startTime time.Time
 }
 
 //
@@ -49,70 +34,95 @@ func (master *Master) setMapReply(reply *MasterMessage) {
 // the RPC argument and reply types are defined in rpc.go.
 //
 func (master *Master) GetTask(args *WorkerMessage, reply *MasterMessage) error {
-	//log.Printf("giving out a task\n")
+	master.GetTaskLock.Lock()
+	defer master.GetTaskLock.Unlock()
 	switch master.phase {
 	case "map":
-		master.lock.Lock()
 		log.Printf("Asking for a map task\n")
-		for taskID, taskStatus := range master.mapTaskStatus {
+		for taskID, taskStatus := range master.mapTasksStatus {
 			if taskStatus.status == "notStarted" {
 				taskStatus.status = "inProgress"
 				taskStatus.startTime = time.Now()
-				master.inProgressMapTasks[taskID] = true
+				master.inProgressTasks[taskID] = true
 
 				reply.Task = "map"
 				reply.File = master.splits[taskID]
 				reply.TaskID = taskID
 				reply.NReduce = master.NReduce
-				master.lock.Unlock()
 				return nil
 			}
 		}
-		master.lock.Unlock()
-
-		// all tasks are either in inProgress(a worker is working on it) or finished state if we come here
-		// we make this worker to run the first reduce, and block until all other map tasks are finished
-		master.wg.Wait()
-		master.phase = "reduce"
-		return nil
 
 	case "reduce":
-
 		log.Printf("Asking for a reduce task\n")
-		reply.Task = "reduce"
+		for taskID, taskStatus := range master.reduceTasksStatus {
+			if taskStatus.status == "notStarted" {
+				taskStatus.status = "inProgress"
+				taskStatus.startTime = time.Now()
+				master.inProgressTasks[taskID] = true
 
-		//if !master.areAllMapTasksDone() {
-		//	reply.Task = "skip"
-		//} else {
-		//	// give out reduce task as we know all map tasks are in finished state
-		//	reply.Task = "reduce"
-		//}
+				reply.Task = "reduce"
+				reply.TaskID = taskID
+				return nil
+			}
+		}
+
+	case "done":
+		reply.Task = "done"
+		return nil
 
 	}
 
+	reply.Task = "idle"
 	return nil
 }
 
-func (master *Master) ReportDone(args *WorkerMessage, reply *MasterMessage) error {
+func (master *Master) ReportDone(workerMssg *WorkerMessage, reply *MasterMessage) error {
+	master.ReportDoneLock.Lock()
+	defer master.ReportDoneLock.Unlock()
 
-	_, isPresent := master.inProgressMapTasks[args.TaskID]
+	_, isPresent := master.inProgressTasks[workerMssg.TaskID]
 
-	// the long running thread that checks for tasks that ran over 10s has already reset the task status to notStarted
+	// time-out checker already reset this task to notStarted
 	if !isPresent {
 		return errors.New("timeout error: task taken longer than 10s")
 	}
 
-	startTime := master.mapTaskStatus[args.TaskID].startTime
-	timeTaken := time.Now().Sub(startTime)
-	if timeTaken.Seconds() > 10 {
-		master.mapTaskStatus[args.TaskID].status = "notStarted"
-		delete(master.inProgressMapTasks, args.TaskID)
-		return errors.New("timeout error: task taken longer than 10s")
-	} else {
-		master.mapTaskStatus[args.TaskID].status = "finished"
-		delete(master.inProgressMapTasks, args.TaskID)
-		master.wg.Done()
+	switch workerMssg.Task {
+	case "map":
+		startTime := master.mapTasksStatus[workerMssg.TaskID].startTime
+		timeTaken := time.Now().Sub(startTime)
+		if timeTaken.Seconds() > 10 {
+			master.mapTasksStatus[workerMssg.TaskID].status = "notStarted"
+			delete(master.inProgressTasks, workerMssg.TaskID)
+			return errors.New("timeout error: task taken longer than 10s")
+		} else {
+			master.mapTasksStatus[workerMssg.TaskID].status = "finished"
+			delete(master.inProgressTasks, workerMssg.TaskID)
+			master.mapTasksTodo -= 1
+			if master.mapTasksTodo == 0 {
+				master.phase = "reduce"
+			}
+		}
+
+	case "reduce":
+		startTime := master.reduceTasksStatus[workerMssg.TaskID].startTime
+		timeTaken := time.Now().Sub(startTime)
+		if timeTaken.Seconds() > 10 {
+			master.reduceTasksStatus[workerMssg.TaskID].status = "notStarted"
+			delete(master.inProgressTasks, workerMssg.TaskID)
+			return errors.New("timeout error: task taken longer than 10s")
+		} else {
+			master.reduceTasksStatus[workerMssg.TaskID].status = "finished"
+			delete(master.inProgressTasks, workerMssg.TaskID)
+			master.reduceTasksTodo -= 1
+			if master.reduceTasksTodo == 0 {
+				master.phase = "done"
+			}
+		}
+
 	}
+
 	return nil
 }
 
@@ -123,9 +133,6 @@ func (master *Master) server() {
 	println("running master.server()")
 	rpc.Register(master)
 	rpc.HandleHTTP()
-	//l, e := net.Listen("tcp", ":1234")
-	//sockname := masterSock()
-	//os.Remove(sockname)
 	l, e := net.Listen("tcp", ":12346")
 	if e != nil {
 		log.Fatal("listen error:", e)
@@ -138,16 +145,20 @@ func (master *Master) server() {
 // if the entire job has finished.
 //
 func (master *Master) Done() bool {
-	ret := false
+	if master.phase == "done" {
+		log.Printf("MapReduce job succeeded!\n")
 
-	// Your code here.
+		// this loop is also important because we will need to have the master alive for some time to
+		// send the done task to all the workers signalling normal termination
+		//for i := 10; i > 0; i-- {
+		//	log.Printf("Master exiting in %vs...\n", i)
+		//	time.Sleep(1 * time.Second)
+		//}
+		//log.Printf("Master shutting down...\n")
+		return true
+	}
 
-	return ret
-}
-
-type MapStatus struct {
-	status    string // notStarted,inProgress, or finished
-	startTime time.Time
+	return false
 }
 
 //
@@ -156,49 +167,51 @@ type MapStatus struct {
 //
 func MakeMaster(files []string, nReduce int) *Master {
 	absFilePaths := toAbsPaths(files)
-	mapTaskStatus := map[int]*MapStatus{}
+	mapTasksStatus := map[int]*TaskStatus{}
+	reduceTasksStatus := map[int]*TaskStatus{}
 
-	// initialize map tasks's status
+	// initialize map tasks' status
 	for i := 0; i < len(absFilePaths); i++ {
-		mapTaskStatus[i] = &MapStatus{
+		mapTasksStatus[i] = &TaskStatus{
+			status:    "notStarted",
+			startTime: time.Time{},
+		}
+	}
+
+	// initialize reduce tasks' status
+	for i := 0; i < nReduce; i++ {
+		reduceTasksStatus[i] = &TaskStatus{
 			status:    "notStarted",
 			startTime: time.Time{},
 		}
 	}
 
 	master := Master{
-		phase:              "map", // first do map then reduce
-		splits:             absFilePaths,
-		NReduce:            nReduce,
-		mapTaskStatus:      mapTaskStatus,
-		inProgressMapTasks: make(map[int]bool),
+		phase:             "map", // first do map then reduce
+		splits:            absFilePaths,
+		NReduce:           nReduce,
+		mapTasksStatus:    mapTasksStatus,
+		reduceTasksStatus: reduceTasksStatus,
+		inProgressTasks:   make(map[int]bool),
+		mapTasksTodo:      len(absFilePaths),
+		reduceTasksTodo:   nReduce,
 	}
 
-	// wait for all map tasks to complete before the first reduce task
-	master.wg.Add(len(master.splits))
-
-	// 0 not started, 1 in progress, 2 finished
-	mapTasks := make(map[string]int)
-
-	for i := 0; i < len(files); i++ {
-		mapTasks[files[i]] = 0
-	}
-
-	// check for tasks overtime every 5 seconds
-	go func() {
+	// start a long running thread to check for timed-out tasks every 5 seconds
+	go func(periodInSeconds int) {
 		for {
 			//log.Printf("Running clean up thread to look for timed-out tasks")
-			for taskID := range master.inProgressMapTasks {
-				timeElapsed := time.Now().Sub(mapTaskStatus[taskID].startTime)
+			for taskID := range master.inProgressTasks {
+				timeElapsed := time.Now().Sub(mapTasksStatus[taskID].startTime)
 				if timeElapsed.Seconds() > 10 {
 					log.Printf("Found a timed-out task: \n")
-					master.mapTaskStatus[taskID].status = "notStarted"
-					delete(master.inProgressMapTasks, taskID)
+					master.mapTasksStatus[taskID].status = "notStarted"
+					delete(master.inProgressTasks, taskID)
 				}
 			}
-			time.Sleep(5 * time.Second)
+			time.Sleep(time.Duration(periodInSeconds) * time.Second)
 		}
-	}()
+	}(5)
 
 	master.server()
 	return &master
